2c27a892f9fae19a62ef2d1add453675745ab966
noa shay <noashay@MacBook-Pro-3.local>
2022-06-12 17:37:48 +0300

    0.1.5

--- setup.py
+++ setup.py
@@ -1,24 +1,34 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
 
 setup(
     name='swip',
-    package_dir={"": "swip"},
-    packages=find_packages(where='swip'),
+    packages=find_packages(where='src'),
+    package_dir={"": "src"},
     include_package_data=True,
+    version='0.1.5',
     license='MIT',
-    version='0.0.1',
-    description='A basic implementation of the version control system Git ',
+    description='swip is a basic version control system, based on Git.',
     author='Noa Shay',
-    author_email='nogaos97@gmail.com',
-    url='https://github.com/ShayNoa/swip',
+    author_email='noa.shay22@gmail.com',
+    url='https://github.com/ShayNoa/swip.git',
+    keywords=['version control', 'git'],
     install_requires=[
         'loguru',
-        'graphviz',
         'colorama',
 	    'termcolor',
-        'wheel'
+        'wheel',
+        'networkx',
+        'matplotlib'
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9'
     ],
     entry_points={
-        'console_scripts': ['swip=swip:main'],
-    }
+        'console_scripts': ['swip=swip.swip:main'],
+    },
 )
